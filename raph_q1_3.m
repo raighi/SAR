c9c00dae51e3b088bc00c848e5a282de1b9de46c
Raphael <raphael.hierso@gmail.com>
2025-05-02 09:50:08 +0200

    Create raph_q1_3.m
    Création du fichuier pour le son de la question 3

---
+++ raph_q1_3.m
@@ -0,0 +1,17 @@
+%Paramètres 
+fe=44100;
+f0=220.455;
+Te=1/fe;
+T=6;
+N=fe*T;
+t=(0:N-1)*Te; %Valeurs de temps d'échantillonage
+
+signal=zeros(1,N);
+for k=1:1:8
+    signal=signal+sin(2*pi*f0*k*t); %Ajout du signal à chaque temps d'échantillonage
+
+end
+
+signal=signal/max(abs(signal)); %Normalisation du signal
+
+audiowrite('singletone_piano_corr3.wav',signal,fe); %Ecriture du fichier son
