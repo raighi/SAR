68e4456a298afb8f1fec876e38263d49f2aa53cd
Raphael <raphael.hierso@gmail.com>
2025-05-02 10:05:15 +0200

    Update raph_q1_3.m

--- raph_q1_3.m
+++ raph_q1_3.m
@@ -7,11 +7,13 @@ N=fe*T;
 t=(0:N-1)*Te; %Valeurs de temps d'échantillonage
 
 signal=zeros(1,N);
-for k=1:1:8
-    signal=signal+sin(2*pi*f0*k*t); %Ajout du signal à chaque temps d'échantillonage
+for k=1:1:7
+    signal=pks1(k)*signal+sin(2*pi*f0*k*t); %Ajout du signal à chaque temps d'échantillonage
 
 end
 
 signal=signal/max(abs(signal)); %Normalisation du signal
 
 audiowrite('singletone_piano_corr3.wav',signal,fe); %Ecriture du fichier son
+
+soundsc(signal,fe)
